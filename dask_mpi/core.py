548556dc2836c7cecb04e4ba6cf11726773a8758
Kevin Paul <kpaul@ucar.edu>
2018-12-19 16:08:52 -0700

    Removing scheduler parameter from initialize

--- dask_mpi/core.py
+++ dask_mpi/core.py
@@ -11,21 +11,20 @@ loop = IOLoop()
 
 
 def initialize(scheduler_file='scheduler.json', interface=None, nthreads=1, local_directory='',
-               memory_limit='auto', scheduler=True, nanny=False, bokeh_port=8787, bokeh_prefix=None,
+               memory_limit='auto', nanny=False, bokeh_port=8787, bokeh_prefix=None,
                bokeh_worker_port=8789):
     host = get_host_from_interface(interface)
 
-    if rank == 0 and scheduler:
+    if rank == 0:
         start_scheduler(loop, host=host, scheduler_file=scheduler_file,
                         bokeh_port=bokeh_port, bokeh_prefix=bokeh_prefix)
         sys.exit()
 
-    elif rank == 1 or (rank == 0 and not scheduler):
+    elif rank == 1:
         pass
 
     else:
-        name = rank-1 if scheduler else None
-        start_worker(loop, host=host, name=name, scheduler_file=scheduler_file, nanny=nanny,
+        start_worker(loop, host=host, name=rank-1, scheduler_file=scheduler_file, nanny=nanny,
                      local_directory=local_directory, nthreads=nthreads, memory_limit=memory_limit,
                      bokeh_worker_port=bokeh_worker_port)
         sys.exit()
