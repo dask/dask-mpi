721351d1fd869eff64b3b7b0f0664c90d6d88a8e
Kevin Paul <kpaul@ucar.edu>
2018-12-20 07:15:45 -0700

    Removing unnecessary import

--- dask_mpi/tests/test_core.py
+++ dask_mpi/tests/test_core.py
@@ -8,11 +8,10 @@ import pytest
 pytest.importorskip('mpi4py')
 
 from distributed.utils import tmpfile
-from distributed.utils_test import loop  # noqa: F401
 
 
-def test_basic(loop):
-    script_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'basic.py')
+def test_basic():
+    script_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'core_basic.py')
     with tmpfile(extension='json') as fn:
         p = subprocess.Popen(['mpirun', '-np', '4', sys.executable, script_file, fn],
                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
