2aadaf10d5841476db3c20d2636210895befabbc
Kevin Paul <kpaul@ucar.edu>
2018-12-27 11:04:35 -0700

    Cleanup and splitting bokeh test into 2 parts, enabling 1 part

--- dask_mpi/tests/test_cli.py
+++ dask_mpi/tests/test_cli.py
@@ -1,14 +1,11 @@
 from __future__ import print_function, division, absolute_import
-import os
-import sys
 
-if os.name == 'posix' and sys.version_info[0] < 3:
-    import subprocess32 as subprocess
-else:
-    import subprocess
+import os
+
 from time import sleep
 
 import pytest
+
 pytest.importorskip('mpi4py')
 
 import requests
@@ -19,12 +16,13 @@ from distributed.utils import tmpfile
 from distributed.utils_test import popen
 from distributed.utils_test import loop  # noqa: F401
 
-FNULL = open(os.devnull, 'w') # hide output of subprocess
+FNULL = open(os.devnull, 'w')  # hide output of subprocess
+
 
 @pytest.mark.parametrize('nanny', ['--nanny', '--no-nanny'])
 def test_basic(loop, nanny):
     with tmpfile(extension='json') as fn:
-        with popen(['mpirun', '--np', '4', 'dask-mpi', '--scheduler-file', fn, nanny], stdin=FNULL):
+        with popen(['mpirun', '-np', '4', 'dask-mpi', '--scheduler-file', fn, nanny]):
             with Client(scheduler_file=fn) as c:
 
                 start = time()
@@ -37,7 +35,7 @@ def test_basic(loop, nanny):
 
 def test_no_scheduler(loop):
     with tmpfile(extension='json') as fn:
-        with popen(['mpirun', '--np', '2', 'dask-mpi', '--scheduler-file', fn], stdin=FNULL):
+        with popen(['mpirun', '-np', '2', 'dask-mpi', '--scheduler-file', fn], stdin=FNULL):
             with Client(scheduler_file=fn) as c:
 
                 start = time()
@@ -46,30 +44,40 @@ def test_no_scheduler(loop):
                     sleep(0.2)
 
                 assert c.submit(lambda x: x + 1, 10).result() == 11
-                with popen(['mpirun', '--np', '1', 'dask-mpi', '--scheduler-file', fn, '--no-scheduler']):
+
+                with popen(['mpirun', '-np', '1', 'dask-mpi', '--scheduler-file', fn, '--no-scheduler']):
 
                     start = time()
                     while len(c.scheduler_info()['workers']) != 2:
                         assert time() < start + 10
                         sleep(0.2)
 
-@pytest.mark.skip
-def test_bokeh(loop):
-    with tmpfile(extension='json') as fn:
-        with popen(['mpirun', '--np', '2', 'dask-mpi', '--scheduler-file', fn,
-                    '--bokeh-port', '59583', '--bokeh-worker-port', '59584'],
-                   stdin=subprocess.DEVNULL):
 
-            for port in [59853, 59584]:
-                start = time()
-                while True:
-                    try:
-                        response = requests.get('http://localhost:%d/status/' % port)
-                        assert response.ok
-                        break
-                    except Exception:
-                        sleep(0.1)
-                        assert time() < start + 20
+def check_port_okay(port):
+    start = time()
+    while True:
+        try:
+            response = requests.get('http://localhost:%d/status/' % port)
+            assert response.ok
+            break
+        except Exception:
+            sleep(0.1)
+            assert time() < start + 20
+
+
+def test_bokeh_scheduler(loop):
+    with tmpfile(extension='json') as fn:
+        with popen(['mpirun', '-np', '2', 'dask-mpi', '--scheduler-file', fn, '--bokeh-port', '59583'],
+                   stdin=FNULL):
+            check_port_okay(59583)
 
         with pytest.raises(Exception):
             requests.get('http://localhost:59583/status/')
+
+
+@pytest.mark.skip
+def test_bokeh_worker(loop):
+    with tmpfile(extension='json') as fn:
+        with popen(['mpirun', '-np', '2', 'dask-mpi', '--scheduler-file', fn, '--bokeh-worker-port', '59584'],
+                   stdin=FNULL):
+            check_port_okay(59584)
