55f12b27cf656c300a63af0f5fb800404a828fbb
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-28 10:02:22 -0700

    Add openmpi jobs to CircleCI workflow

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -7,7 +7,7 @@ apt-get update; apt-get install -y make
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda update -q conda
 conda config --add channels conda-forge
-conda env create -f ci/environment-dev-${PYTHON}.yml --name=${ENV_NAME} --quiet
+conda env create -f ci/environment-dev-${PYTHON}-${MPI}.yml --name=${ENV_NAME} --quiet
 conda env list
 source activate ${ENV_NAME}
 pip install pip --upgrade
