dd164b43b44a18502f744cea789bf74c879e7c1d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-28 16:16:57 -0700

    Restart shell after conda init bash

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -8,6 +8,7 @@ export PATH="/opt/conda/bin:${PATH}"
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda update -q conda
 conda init bash # Initialize bash shell, this is new in conda 4.6
+exec bash # Need to restart the shell after conda init bash
 conda config --set pip_interop_enabled True # Enable pip interoperability
 conda config --add channels conda-forge
 conda env create -f ci/environment-dev-${PYTHON}-${MPI}.yml --name=${ENV_NAME} --quiet
