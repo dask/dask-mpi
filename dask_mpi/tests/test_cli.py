78976633eeb90ecbc38976f30110a1b1067c80c5
Lehman Garrison <lgarrison@flatironinstitute.org>
2022-10-05 14:47:26 -0400

    Fix tests, following dask/dask#9337

--- dask_mpi/tests/test_cli.py
+++ dask_mpi/tests/test_cli.py
@@ -12,7 +12,7 @@ from distributed import Client
 from distributed.comm.addressing import get_address_host_port
 from distributed.metrics import time
 from distributed.utils import import_term, tmpfile
-from distributed.utils_test import loop  # noqa: F401
+from distributed.utils_test import loop, loop_in_thread, cleanup  # noqa: F401
 from distributed.utils_test import popen
 
 pytest.importorskip("mpi4py")
