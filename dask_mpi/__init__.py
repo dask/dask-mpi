08abb5aad32d8b507c0d95e38ba3931e67ec29c6
Jacob Tomlinson <jtomlinson@nvidia.com>
2023-09-18 16:33:00 +0100

    Expose exception at module level

--- dask_mpi/__init__.py
+++ dask_mpi/__init__.py
@@ -1,5 +1,6 @@
 from ._version import get_versions
 from .core import initialize, send_close_signal
+from .exceptions import WorldTooSmallException
 
 __version__ = get_versions()["version"]
 del get_versions
