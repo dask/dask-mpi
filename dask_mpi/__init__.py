a35c26179792bf1534d166e5db43a997d6c2d0d0
Kevin Paul <kpaul@nvidia.com>
2023-10-13 11:34:19 +0200

    send_close_signal has moved

--- dask_mpi/__init__.py
+++ dask_mpi/__init__.py
@@ -1,7 +1,7 @@
 from ._version import get_versions
 from .exceptions import WorldTooSmallException  # noqa
-from .execute import execute  # noqa
-from .initialize import initialize, send_close_signal  # noqa
+from .execute import execute, send_close_signal  # noqa
+from .initialize import initialize  # noqa
 
 __version__ = get_versions()["version"]
 del get_versions
