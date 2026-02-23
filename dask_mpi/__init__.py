6edb128bd82d9716f7e11014d09d0cc3836218ba
Jacob Tomlinson <jtomlinson@nvidia.com>
2026-02-23 16:57:56 +0000

    Migrate to pyproject.toml

--- dask_mpi/__init__.py
+++ dask_mpi/__init__.py
@@ -1,6 +1,7 @@
-from ._version import get_versions
+from importlib.metadata import version
+
 from .exceptions import WorldTooSmallException  # noqa
 from .execute import execute, send_close_signal  # noqa
 from .initialize import initialize  # noqa
 
-__version__ = _version.get_versions()["version"]
+__version__ = version("dask-mpi")
