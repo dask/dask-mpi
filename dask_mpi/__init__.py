b48d2535286ba8809516024b503d1911243c0a4b
Jacob Tomlinson <jtomlinson@nvidia.com>
2026-02-24 11:07:20 +0000

    Ensure version is built into distributions

--- dask_mpi/__init__.py
+++ dask_mpi/__init__.py
@@ -1,7 +1,5 @@
-from importlib.metadata import version
-
 from .exceptions import WorldTooSmallException  # noqa
 from .execute import execute, send_close_signal  # noqa
 from .initialize import initialize  # noqa
 
-__version__ = version("dask-mpi")
+from ._version import __version__
